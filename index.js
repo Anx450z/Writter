44982f4357c23e9f887595b501e3f2fc10f814e8
Ankur <anx450z@gmail.com>
2023-06-06 15:25:15 +0530

    working playback

--- index.js
+++ index.js
@@ -17,9 +17,19 @@ function playbackKeystrokes() {
 
   Object.keys(keystrokesHash).forEach(function (timestamp, index) {
     timeDelay = timestamp - Object.keys(keystrokesHash)[0]
-    console.log(timeDelay, index, timestamp)
     setTimeout(function () {
-      outputTextBox.value += keystrokesHash[timestamp];
+      var key = keystrokesHash[timestamp];
+      if (key === 'Enter') {
+        outputTextBox.value += '\n';
+      } else if (key === 'Backspace') {
+        outputTextBox.value = outputTextBox.value.slice(0, -1);
+      } else if (key === 'Tab') {
+        outputTextBox.value += '\t';
+      } else if (key === 'Shift') {
+      } 
+      else {
+        outputTextBox.value += key;
+      }
     }, timeDelay);
   });
 }
